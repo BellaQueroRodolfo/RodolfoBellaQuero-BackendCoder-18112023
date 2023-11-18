1e665ad45078acfb3af49f6c534f4292964c1fff
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-11-18 19:16:46 -0300

    Create authRouter.js

---
+++ src/routes/authRouter.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const User = require('./models/User');
+const authRouter = express.Router();
+
+authRouter.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+authRouter.post('/login', passport.authenticate('local', {
+  successRedirect: '/products',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+authRouter.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
+module.exports = authRouter;
