823adfc7147d49fa5e271aa8853109ef90280951
BellaQueroRodolfo <83620265+BellaQueroRodolfo@users.noreply.github.com>
2023-11-18 19:09:01 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,55 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const http = require('http');
-const socketIo = require('socket.io');
-const mongoose = require('mongoose'); // Import Mongoose
-
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
 const port = 8080;
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const productsRouter = require('./routes/product');
-const cartsRouter = require('./routes/cart');
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
-app.use('/products', productsRouter);
-app.use('/carts', cartsRouter);
-app.get('/chat', (req, res) => {
-  res.render('chat', { layout: 'main' });
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-io.on('connection', (socket) => {
-  socket.on('new-product', (product) => {
-    io.emit('product-added', product);
-  });
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
 });
 
 mongoose.connect('mongodb://localhost:27017/ecommerce', {
@@ -45,6 +67,17 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const productsRouter = require('./src/routes/products');
+const cartsRouter = require('./src/routes/carts');
+const authRouter = require('./src/routes/auth');
+
+app.use('/auth', authRouter);
+
+const customRouter = require('./src/routes/custom');
+app.use('/custom', customRouter);
+
+const apiRouter = require('./src/routes/api');
+app.use('/api', apiRouter);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
